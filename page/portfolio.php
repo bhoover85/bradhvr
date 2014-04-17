b1204cb4eee33fde97278c37345c109bb73166fe
Brad Hoover <hoov1185@gmail.com>
2014-04-17 14:14:38 -0400

    Changed link behavior. Changed project titles to links.

--- page/portfolio.php
+++ page/portfolio.php
@@ -4,33 +4,33 @@
 <div class="row">
 
 	<div class="col-md-4 col-sm-6 col-xs-12 text-center">
-		<a href="portfolio/casualties-of-the-gridiron"><img src="img/portfolio/cog.jpg" class="img-responsive grayscale"></a>
-		<p class="project-title">Casualties of the Gridiron</p>
+		<a href="portfolio/casualties-of-the-gridiron"><img src="img/portfolio/cog.jpg" class="img-responsive grayscale">
+		<p class="project-title">Casualties of the Gridiron</p></a>
 	</div>
 
 	<div class="col-md-4 col-sm-6 col-xs-12 text-center">
-		<a href="portfolio/ux-entertainment"><img src="img/portfolio/uxe.jpg" class="img-responsive grayscale"></a>
-		<p class="project-title">UX Entertainment</p>
+		<a href="portfolio/ux-entertainment"><img src="img/portfolio/uxe.jpg" class="img-responsive grayscale">
+		<p class="project-title">UX Entertainment</p></a>
 	</div>
 
 	<div class="col-md-4 col-sm-6 col-xs-12 text-center">
-		<a href="portfolio/revival-41"><img src="img/portfolio/revival41.jpg" class="img-responsive grayscale"></a>
-		<p class="project-title">REvival 41</p>
+		<a href="portfolio/revival-41"><img src="img/portfolio/revival41.jpg" class="img-responsive grayscale">
+		<p class="project-title">REvival 41</p></a>
 	</div>
 
 	<div class="col-md-4 col-sm-6 col-xs-12 text-center">
-		<a href="portfolio/thin-crust-square"><img src="img/portfolio/tcs.jpg" class="img-responsive grayscale"></a>
-		<p class="project-title">Thin Crust Square</p>
+		<a href="portfolio/thin-crust-square"><img src="img/portfolio/tcs.jpg" class="img-responsive grayscale">
+		<p class="project-title">Thin Crust Square</p></a>
 	</div>
 
 	<div class="col-md-4 col-sm-6 col-xs-12 text-center">
-		<a href="portfolio/gem-archive-search"><img src="img/portfolio/gem.jpg" class="img-responsive grayscale"></a>
-		<p class="project-title">GEM Archive Search</p>
+		<a href="portfolio/gem-archive-search"><img src="img/portfolio/gem.jpg" class="img-responsive grayscale">
+		<p class="project-title">GEM Archive Search</p></a>
 	</div>
 
 	<div class="col-md-4 col-sm-6 col-xs-12 text-center">
-		<a href="portfolio/rfid-tracking-system"><img src="img/portfolio/rfid.jpg" class="img-responsive grayscale"></a>
-		<p class="project-title">RFID Tracking System</p>
+		<a href="portfolio/rfid-tracking-system"><img src="img/portfolio/rfid.jpg" class="img-responsive grayscale">
+		<p class="project-title">RFID Tracking System</p></a>
 	</div>
 
 </div>
